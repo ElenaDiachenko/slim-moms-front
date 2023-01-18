b5c55980eb49c9bb96bad9f4e2755d018a2b1f9c
ElenaDiachenko <96205320+ElenaDiachenko@users.noreply.github.com>
2023-01-18 03:04:54 +0200

    changed path

--- src/servises/instanceAxios.js
+++ src/servises/instanceAxios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_URL = `https://slim-moms.onrender.com/api/`;
+export const API_URL = `https://slimmoms-api.onrender.com/api/`;
 
 const $api = axios.create({
   withCredentials: true,
